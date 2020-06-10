18cdf6ae691c439858b3b7b128136a30b743b944
Donat Avkhadiev <sir.mailwork@gmail.com>
2020-06-09 20:56:02 -0400

    hw to do

---
+++ hw_1.py
@@ -0,0 +1,34 @@
+# 1
+
+# n = int(input("Enter a random string, which includes only digits: "))
+# sum = 0
+# while n != 0:
+#     sum = sum + n % 10
+#     n = n // 10
+# print("Sum of digits: ", sum)
+
+#2
+# list1 = []
+#
+# for i in range(3):
+#     number = int(input("Enter numbers: "))
+#     list1.append(number)
+#
+# print("Largest number is:", max(list1))
+
+#3
+
+# n = int(input('Enter the number: '))
+# s = 0
+# even = 0
+# odd = 0
+# while n != 0:
+#     s = n % 10
+#     if s % 2 == 0:
+#         even += 1
+#     else:
+#         odd += 1
+#
+#     n = n // 10
+# print(even)
+# print(odd)
